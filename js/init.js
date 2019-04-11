61616ca5650564c0944654373eca2b4974dfc381
Vicente Lucas <vica.escorcio@gmail.com>
2019-04-11 10:23:44 -0300

    carousel

--- js/init.js
+++ js/init.js
@@ -3,6 +3,14 @@
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
+    $('.carousel').carousel({
+      indicators: true,
+      numVisible: 2,
+      onCycleTo: function(){
+        $('.fade_section').fadeToggle("200");
+      }
+    });
+
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
